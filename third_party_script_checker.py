025a28436e7d84c168e98c3bffee46e12fbd1ca3
anh <ngducanh126@gmail.com>
2025-04-25 16:47:18 -0400

    added implementation of find_scripts_without_sri in third_party_script_checker.py

--- third_party_script_checker.py
+++ third_party_script_checker.py
@@ -11,3 +11,16 @@ def find_external_scripts_in_html(directory):
                             found.append((path, i+1, line.strip()))
     return found
 
+def find_scripts_without_sri(directory):
+    import os
+    found = []
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            if f.endswith(".html"):
+                path = os.path.join(root, f)
+                with open(path, "r", encoding="utf-8") as file:
+                    for i, line in enumerate(file):
+                        if "<script" in line and "src=" in line and "integrity=" not in line:
+                            found.append((path, i+1, line.strip()))
+    return found
+
