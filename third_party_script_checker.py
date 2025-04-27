4fefc8611e39d836534965568ac7c214fe426f92
anh <ngducanh126@gmail.com>
2025-04-27 15:58:48 -0400

    added implementation of find_css_without_sri in third_party_script_checker.py

--- third_party_script_checker.py
+++ third_party_script_checker.py
@@ -37,3 +37,16 @@ def find_external_css_in_html(directory):
                             found.append((path, i+1, line.strip()))
     return found
 
+def find_css_without_sri(directory):
+    import os
+    found = []
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            if f.endswith(".html"):
+                path = os.path.join(root, f)
+                with open(path, "r", encoding="utf-8") as file:
+                    for i, line in enumerate(file):
+                        if "<link" in line and "href=" in line and "stylesheet" in line and "integrity=" not in line:
+                            found.append((path, i+1, line.strip()))
+    return found
+
